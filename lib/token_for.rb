f773a8aeba6e6a91aebf27ac63167d9370a3ed58
Janusz Pietrzyk <yanozsick@gmail.com>
2014-06-09 17:45:28 +0200

    remove binding.pry

--- lib/token_for.rb
+++ lib/token_for.rb
@@ -11,7 +11,6 @@ module TokenFor
       define_method("#{subject}_token") do
         verifier = self.class.verifier_for(subject)
         attr_hash = {} # The attributes need to be in a hash
-        binding.pry
         attrs.map { |a| attr_hash.merge! a.to_sym => self.read_attribute(a) }
         attr_hash.merge!(expires_on: Time.zone.now + expires_in) if expires_in
         # Generate based on actual values of attributes.
